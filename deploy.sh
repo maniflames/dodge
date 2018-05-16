9f5885522fb46a3cbfd5417e62e5ea678ccca41b
Imani Dap <imani.dap@gmail.com>
2018-05-16 22:23:04 +0200

    travis: trying another auth method

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,16 @@
 
 set -e #quit on error
 
-sshpass -p $PASS ssh -o StrictHostKeyChecking=no $USERHOST
-echo "Travis got tha power" >> hello.txt
-exit 
+#sshpass -p $PASS ssh -o StrictHostKeyChecking=no $USERHOST
+#echo "Travis got tha power" >> hello.txt
+#exit
+
+#Found this gem: https://gist.github.com/nickbclifford/16c5be884c8a15dca02dca09f65f97bd
+#I had some struggles tho, this stuff helped me as well
+#https://www.ssh.com/ssh/copy-id
+#https://docs.travis-ci.com/user/encrypting-files/
+#Never run travis encrypt-file with sudo!!
+
+ssh $USER@$HOST <<EOF
+  echo "Travis got tha power" >> hello.txt
+EOF
